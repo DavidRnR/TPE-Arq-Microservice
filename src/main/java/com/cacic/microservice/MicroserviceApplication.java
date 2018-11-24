1b5144e6d8fb5f20af0d641dea6bc615315cbb37
unknown <carlosdavidmartin@gmail.com>
2018-11-23 21:45:44 -0300

    Config Deployment

--- src/main/java/com/cacic/microservice/MicroserviceApplication.java
+++ src/main/java/com/cacic/microservice/MicroserviceApplication.java
@@ -1,11 +1,19 @@
 package com.cacic.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroserviceApplication {
+public class MicroserviceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MicroserviceApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
+
 }
